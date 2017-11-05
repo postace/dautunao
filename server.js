a8c0f2a2bc1435aed181559b474eeffd76e753ac
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-11-05 17:15:33 +0700

    Setup ORM and connect to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('dotenv').load();
-
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.Promise = global.Promise;
 
 
 const port = process.env.PORT || 3000;
